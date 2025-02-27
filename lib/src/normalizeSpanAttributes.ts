049d42d6128d6d539e31c97b214815bf2063e644
patricklafrance <pa.lafrance@gmail.com>
2025-02-27 14:46:30 -0500

    More stuff

---
+++ lib/src/normalizeSpanAttributes.ts
@@ -0,0 +1,48 @@
+import type { FetchCustomAttributeFunction, FetchInstrumentationConfig } from "@opentelemetry/instrumentation-fetch";
+import type { XHRCustomAttributeFunction, XMLHttpRequestInstrumentationConfig } from "@opentelemetry/instrumentation-xml-http-request";
+
+export function _normalXmlHttpInstrumentationSpanAttributes(config: XMLHttpRequestInstrumentationConfig, normalizeFunction: XHRCustomAttributeFunction) {
+    if (config.applyCustomAttributesOnSpan) {
+        const baseFunction = config.applyCustomAttributesOnSpan;
+
+        config.applyCustomAttributesOnSpan = (...args) => {
+            baseFunction(...args);
+            normalizeFunction(...args);
+        };
+    } else {
+        config.applyCustomAttributesOnSpan = normalizeFunction;
+    }
+
+    return config;
+}
+
+export function normalXmlHttpInstrumentationSpanAttributes(config: XMLHttpRequestInstrumentationConfig) {
+    return _normalXmlHttpInstrumentationSpanAttributes(config, (span, xhr) => {
+        span.setAttribute("http.request.method", xhr.responseURL);
+        span.setAttribute("http.response.status_code", xhr.status);
+        span.setAttribute("url.full", xhr.responseURL);
+    });
+}
+
+export function _normalizeFetchInstrumentationSpanAttributes(config: FetchInstrumentationConfig, normalizeFunction: FetchCustomAttributeFunction) {
+    if (config.applyCustomAttributesOnSpan) {
+        const baseFunction = config.applyCustomAttributesOnSpan;
+
+        config.applyCustomAttributesOnSpan = (...args) => {
+            baseFunction(...args);
+            normalizeFunction(...args);
+        };
+    } else {
+        config.applyCustomAttributesOnSpan = normalizeFunction;
+    }
+
+    return config;
+}
+
+export function normalizeFetchInstrumentationSpanAttributes(config: FetchInstrumentationConfig) {
+    return _normalizeFetchInstrumentationSpanAttributes(config, (span, request, result) => {
+        span.setAttribute("http.request.method", request.method ?? "GET");
+        span.setAttribute("http.response.status_code", result.status ?? 0);
+        span.setAttribute("url.full", (request as Request).url);
+    });
+}
